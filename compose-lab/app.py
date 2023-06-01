66e06737e7283f65a07befd8c9dcbd2b6e0720f0
Tim Warner <timothy-warner@pluralsight.com>
2023-06-01 10:58:55 -0500

    replace sample app

--- compose-lab/app.py
+++ compose-lab/app.py
@@ -1,22 +1,34 @@
-from flask import Flask, request, render_template, redirect
-from redis import Redis
+from flask import Flask, request, redirect, url_for
+import redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+r = redis.Redis(host='redis', port=6379, db=0)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def todo():
     if request.method == 'POST':
-        item = request.form.get('item')
-        action = request.form.get('action')
+        task = request.form.get('task')
+        r.rpush('tasks', task)
+    tasks = [task.decode('utf-8') for task in r.lrange('tasks', 0, -1)]
+    tasks_html = ''.join(
+        f'<li>{task} <form method="post" action="{url_for("delete")}"><input type="hidden" name="task" value="{task}"><input type="submit" value="Delete"></form></li>' for task in tasks)
+    return f'''
+        <h1>Todo List</h1>
+        <form method="post">
+            <input type="text" name="task" required>
+            <input type="submit" value="Add Task">
+        </form>
+        <ul>{tasks_html}</ul>
+    '''
 
-        if action == 'Add':
-            redis.rpush('todos', item)
-        elif action == 'Remove':
-            redis.lrem('todos', 1, item)
 
-    items = redis.lrange('todos', 0, -1)
-    return render_template('index.html', items=items)
+@app.route('/delete', methods=['POST'])
+def delete():
+    task = request.form.get('task')
+    r.lrem('tasks', 1, task.encode('utf-8'))
+    return redirect(url_for('todo'))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
